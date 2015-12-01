f0ba18aa4bec8524775c3bb7c48b393945f1b84d
Vitor Aguiar <vitor.aguiar@me.com>
2015-12-01 01:17:02 -0200

    day 2 code

---
+++ R/dplyr_tidyr_code.R
@@ -0,0 +1,132 @@
+library(magrittr)
+
+# magrittr operators: %>% and %<>%
+
+#1 R base
+class(iris)
+
+#1 magrittr
+iris %>% class()
+
+#2 R base
+grep("Petal", names(iris))
+
+#2 magrittr
+names(iris) %>% grep("Petal", .)
+
+#3 R base
+sapply(as.data.frame(list(a = 1:10, b = rbinom(10, 1, .5), c = rnorm(10))), mean)
+
+#3 magrittr
+list(a = 1:10, b = rbinom(10, 1, .5), c = rnorm(10)) %>%
+  as.data.frame() %>%
+  sapply(mean)
+
+#4 R base
+x <- 10
+x <- log10(x) 
+x
+
+#4 magrittr
+x <- 10
+x %<>% log10()
+
+#5 R base
+iris <- iris[iris$Species == "virginica", ]
+
+#5 magrittr
+iris %<>% .[.$Species == "virginica", ]
+
+
+#### dplyr --------------------------------------------------------------------#
+data(msleep, package = "ggplot2")
+
+str(msleep)
+
+msleep %>% count(order)
+
+msleep %>% filter(order == "Carnivora" & !is.na(brainwt))
+
+msleep %>% 
+  filter(order == "Carnivora" & !is.na(brainwt)) %>%
+  select(genus, bodywt)
+
+msleep %>%
+  group_by(order) %>%
+  summarise(average_sleep = mean(sleep_total))
+
+msleep %>% 
+  group_by(order, vore) %>%
+  summarise(average_sleep = mean(sleep_total), average_bodywt = mean(bodywt))
+
+msleep %>%
+  group_by(order) %>%
+  summarise_each(funs(mean(., na.rm = TRUE)), sleep_total, brainwt, bodywt)
+
+msleep %>%
+  group_by(order) %>%
+  summarise_each(funs(mean(., na.rm = TRUE), median(., na.rm = TRUE)), 
+                 sleep_total, brainwt, bodywt)
+
+msleep %>%
+  select(genus, sleep_total, awake) %>%
+  mutate(prop_sleep_awake = sleep_total/awake)
+
+msleep %>%
+  select(genus, bodywt) %>%
+  mutate(bodywt = round(bodywt*2.20462))
+
+msleep %>%
+  select(genus, sleep_total, awake) %>%
+  mutate_each(funs(./24), sleep_total, awake)
+
+msleep %>%
+  select(genus, sleep_total, awake) %>%
+  mutate_each(funs(./24), sleep_total, awake) %>%
+  arrange(sleep_total, awake, genus)
+
+msleep %>%
+  select(genus, sleep_total, awake) %>%
+  mutate_each(funs(./24), sleep_total, awake) %>%
+  arrange(sleep_total, awake, genus) %>%
+  rename(prop_sleep = sleep_total)
+
+
+#### tidyr --------------------------------------------------------------------#
+
+monkeys <- paste(sample(1:20), c(rep("ES", 10), rep("MG", 10)), sep = "_")
+
+monkey_wt <- data_frame(individual = monkeys,
+                        "2013/01/01" = rnorm(20, 5, sd = 1),
+                        "2014/01/14" = rnorm(20, 7, sd = 1.5),
+                        "2015/02/02" = rnorm(20, 10, sd = 2))
+
+monkey_wt
+
+library(magrittr)
+library(tidyr)
+
+monkey_wt %<>%
+  gather(key=date, value=weight, 2:4)
+
+monkey_wt
+
+monkey_wt %>% spread(date, weight)
+
+monkey_wt %<>%  
+  separate(individual, into = c("id_number", "state"), sep = "_") %>%
+  mutate(id_number = as.numeric(id_number)) %>%
+  arrange(id_number, state)
+
+monkey_wt
+
+monkey_wt %<>%
+  extract(date, c("year", "month"), "(\\d+)/(\\d+)")
+
+monkey_wt
+
+monkey_wt %>% unite(date, year:month, sep = "/")
+
+monkey_wt %>% 
+  filter(state == "ES") %>% 
+  spread(id_number, weight)
